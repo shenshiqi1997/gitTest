8e36d9bac4f15899421a4adbe5e81872ee9bddb3
ShenShiQi <1175542797@qq.com>
2021-07-16 10:55:51 +0800

    hot-fix

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -5,5 +5,6 @@ public class gitTest {
         System.out.println("hello git ");
         System.out.println("hello git 3");
         System.out.println("hello git 4");
+        System.out.println("hello git 5");
     }
 }
